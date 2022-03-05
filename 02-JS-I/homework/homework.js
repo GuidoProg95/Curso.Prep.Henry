928ecf631ec2224c9d797ce62db3320880814e64
guido <guido_emma@hotmail.com>
2022-03-05 12:02:12 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -5,7 +5,6 @@ const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
-
 // Crea una variable booleana:
 const nuevoBool = null;
 
@@ -15,7 +14,7 @@ const nuevaResta = 10 - null === 5;
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
-// Resuelve el siguiente problema matemático:
+// Resuelve el siguiente problema matemático
 const nuevoModulo = 21 % 5 === null;
 
 
